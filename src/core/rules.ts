abc87b27dd827cfc8dba4b1ef53eb60c842efca7
Ayo Richard ABE [gODtECH] <ayoabe24@gmail.com>
2026-09-12 12:27:53 +0100

    refactor: route rule execution through rule packs

--- src/core/rules.ts
+++ src/core/rules.ts
@@ -1,21 +1,15 @@
 import type { StewardRule } from "./types.js";
-import { dependenciesRule } from "../rules/dependencies.js";
-import { documentationRule } from "../rules/documentation.js";
-import { hygieneRule } from "../rules/hygiene.js";
-import { maintenanceRule } from "../rules/maintenance.js";
-import { repositoryRule } from "../rules/repository.js";
-import { securityRule } from "../rules/security.js";
+import { enabledRulePacks, RULE_PACKS, rulePackSummaries, rulesForPacks } from "./rule-packs.js";
 
-export const RULES: readonly StewardRule[] = [
-  repositoryRule,
-  securityRule,
-  documentationRule,
-  dependenciesRule,
-  maintenanceRule,
-  hygieneRule
-];
+export { RULE_PACKS };
 
-export function enabledRules(disabled: readonly string[]): readonly StewardRule[] {
-  const excluded = new Set(disabled);
-  return RULES.filter((rule) => !excluded.has(rule.id));
+export function enabledRules(
+  disabledRules: readonly string[],
+  disabledPacks: readonly string[] = [],
+): readonly StewardRule[] {
+  return rulesForPacks(enabledRulePacks(disabledPacks), disabledRules);
+}
+
+export function enabledPackSummaries(disabledPacks: readonly string[] = []) {
+  return rulePackSummaries(enabledRulePacks(disabledPacks));
 }
