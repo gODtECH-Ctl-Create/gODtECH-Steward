3f9358a0d876c93aaa08df4837430a7704238cef
Ayo Richard ABE [gODtECH] <ayoabe24@gmail.com>
2026-09-12 10:38:00 +0100

    feat: add first-class Steward rule registry

---
+++ src/core/rules.ts
@@ -0,0 +1,19 @@
+import type { StewardRule } from "./types.js";
+import { documentationRule } from "../rules/documentation.js";
+import { dependenciesRule } from "../rules/dependencies.js";
+import { hygieneRule } from "../rules/hygiene.js";
+import { repositoryRule } from "../rules/repository.js";
+import { securityRule } from "../rules/security.js";
+
+export const RULES: readonly StewardRule[] = [
+  repositoryRule,
+  securityRule,
+  documentationRule,
+  dependenciesRule,
+  hygieneRule
+];
+
+export function enabledRules(disabled: readonly string[]): readonly StewardRule[] {
+  const excluded = new Set(disabled);
+  return RULES.filter((rule) => !excluded.has(rule.id));
+}
